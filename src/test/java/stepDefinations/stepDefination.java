e7b0fad27b82663087f73095901b1324aadfe37e
amit-automationQA <kundaramit@gmail.com>
2022-01-04 13:33:03 +0530

    Logging implemented

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -1,55 +1,32 @@
 package stepDefinations;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import pojo.AddPlace;
-import pojo.Location;
+import resources.TestDataBuild;
+import resources.Utils;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.*;
-public class stepDefination {
+
+import java.io.FileNotFoundException;
+public class stepDefination extends Utils {
 	ResponseSpecification resspec;
 	RequestSpecification res;
 	Response response;
+	TestDataBuild td = new TestDataBuild();
 	@Given("Add Place payload")
-	public void add_place_payload() {
-		RestAssured.baseURI="https://rahulshettyacademy.com";
-
-		AddPlace p =new AddPlace();
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setPhone_number("(+91) 983 893 3937");
-		p.setWebsite("https://rahulshettyacademy.com");
-		p.setName("Frontline house");
-		List<String> myList =new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-
-		p.setTypes(myList);
-		Location l =new Location();
-		l.setLat(-38.383494);
-		l.setLng(33.427362);
-		p.setLocation(l);
-
-		RequestSpecification req =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-				.setContentType(ContentType.JSON).build();
-
+	public void add_place_payload() throws FileNotFoundException {
 
 		resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		res=given().spec(req)
-				.body(p);
+		res=given().spec(requestSpecification())
+				.body(td.addPlacePayload());
 
 	}
 	@When("user calls {string} with POST http request")
