d1539b1a980bed21cc514aa00ee32d23e712430f
amit-automationQA <kundaramit@gmail.com>
2022-01-04 21:58:25 +0530

    Report implemented

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features", glue={"stepDefinations"}, tags= "@DeletePlace")
+@CucumberOptions(features="src/test/java/features", glue={"stepDefinations"}, plugin="json:target/jsonReports/cucumber-report.json")
 public class TestRunner {
-	
+	//tags= "@DeletePlace"
 }
